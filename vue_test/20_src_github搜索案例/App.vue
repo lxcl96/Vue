c09f8bf1a17cccaceefa5517c6a5fa6b138f6dda
lxcl96 <283940766@qq.com>
2023-09-12 16:01:22 +0800

    github搜索案例

---
+++ vue_test/20_src_github搜索案例/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <user-search />
+    <user-list />
+  </div>
+</template>
+    
+<script>
+import UserSearch from './components/UserSearch.vue'
+import UserList from './components/UserList.vue'
+export default {
+    name: 'App',
+    components: {UserSearch,UserList}
+}
+</script>
+
+
+<style>
+/*  <style src='../public/css/bootstrap.css'> */
+/* @import '../public/bootstrap.css'; */
+</style>
